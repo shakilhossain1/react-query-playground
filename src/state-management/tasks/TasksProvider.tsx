0615635c80079c1d1196f6b1dd8809bed33286c1
shakil hossain <sh9743441@gmail.com>
2025-09-16 15:11:20 +0600

    refactor

--- src/state-management/tasks/TasksProvider.tsx
+++ src/state-management/tasks/TasksProvider.tsx
@@ -1,3 +1,6 @@
+import { ReactNode, useReducer } from "react";
+import TasksContext from "./tasksContext";
+
 export interface Task {
   id: number;
   title: string;
@@ -24,4 +27,18 @@ const taskReducer = (tasks: Task[], action: TaskAction): Task[] => {
   }
 };
 
-export default taskReducer;
+interface Props {
+  children: ReactNode;
+}
+
+const TasksProvider = ({ children }: Props) => {
+  const [tasks, dispatch] = useReducer(taskReducer, []);
+
+  return (
+    <TasksContext.Provider value={{ tasks, dispatch }}>
+      {children}
+    </TasksContext.Provider>
+  );
+};
+
+export default TasksProvider;
